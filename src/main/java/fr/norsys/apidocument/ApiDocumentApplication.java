33f0f6656afc937f3e221177b2dc97cc3ace7df0
Ayoub <essadeqayoub14@gmail.com>
2024-04-15 10:32:53 +0100

    init commit

---
+++ src/main/java/fr/norsys/apidocument/ApiDocumentApplication.java
@@ -0,0 +1,13 @@
+package fr.norsys.apidocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/norsys/apidocument/ApiDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.norsys.apidocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
